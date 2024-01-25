21880794dcb44d585aaa1bce626b34c77594e258
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-01-25 22:05:23 +0530

    building homepage card mobile view

--- Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
+++ Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31d415f86cfa3b0cc5ac2e69a3f74b6c562588cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3f69bbe79a368491fc0990b56d413465b054f88")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
