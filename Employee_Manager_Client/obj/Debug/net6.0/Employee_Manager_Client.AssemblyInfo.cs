25dc5586c464ad74f27eced4e879463be8e78602
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-02-02 21:10:41 +0530

    login status message added in login page

--- Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
+++ Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f17abb7f9e8b4e1dcad5b7e96f7f4d10a67af8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5009d963893aebeafa48b0942717b18807c54476")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
