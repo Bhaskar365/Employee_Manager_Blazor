e853e204fce7fce442d0f7e0f026203c0c418d26
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-02-05 19:52:18 +0530

    small changes for logout function

--- Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
+++ Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96c149ae423995897694cf5825d614815d4b4ed4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5afa7d4070611679b9df361b48dec18dfb7cb7a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
