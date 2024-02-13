0dd04f9c74669d18bd291d1d845d2dc4a16cbd50
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-02-13 21:44:19 +0530

    delete button modal working

--- Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
+++ Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a1accefd12d1b260387fa9fec6c462c591649f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecda3b5b66de8ceeb22af554b63e879518760e69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
