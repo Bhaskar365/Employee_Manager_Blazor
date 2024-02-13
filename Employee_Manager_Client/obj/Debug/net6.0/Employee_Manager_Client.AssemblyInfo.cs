ecda3b5b66de8ceeb22af554b63e879518760e69
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-02-13 19:21:13 +0530

    minor button css in all razor pages

--- Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
+++ Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37a1fd28d3eb4aa11e0ec427d139f74c71d4db88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a1accefd12d1b260387fa9fec6c462c591649f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Employee_Manager_Models/obj/Debug/net6.0/Employee_Manager_Models.AssemblyInfo.cs
+++ Employee_Manager_Models/obj/Debug/net6.0/Employee_Manager_Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee_Manager_Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4250b1172662c491f150d4bf79b1a5ca7e75e56a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a1accefd12d1b260387fa9fec6c462c591649f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee_Manager_Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee_Manager_Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
