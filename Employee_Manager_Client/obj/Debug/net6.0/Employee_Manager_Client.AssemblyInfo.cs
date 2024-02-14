7016bef7e388678b20ed7c0dbb0be18edfe903cb
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-02-14 21:13:00 +0530

    search code changes made

--- Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
+++ Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b8e5316d8e052b2322ad741e0ca427da87d18ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8c3f80312bad21851393286ed7625915dc39fcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
