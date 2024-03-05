f866e743c6d8806f5e0a1cae1944966f25756688
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-03-05 21:19:09 +0530

    changing api service class by implementing clean interface class architecture

--- Employee_Manager_Client/Services/ApiService.cs
+++ Employee_Manager_Client/Services/ApiService.cs
@@ -9,179 +9,179 @@ using System.Net.Http.Json;
 
 namespace Employee_Manager_Client.Services
 {
-    public class ApiService
-    {
-        private readonly HttpClient _httpClient;
+    //public class ApiService
+    //{
+    //    private readonly HttpClient _httpClient;
 
-        public ApiService(HttpClient httpClient)
-        {
-            this._httpClient = httpClient;
-        }
+    //    public ApiService(HttpClient httpClient)
+    //    {
+    //        this._httpClient = httpClient;
+    //    }
 
-        //LOGIN REGISTER SERVICE
-        public async Task<ResponseModel> Login(LoginModel loginModel)
-        {
-            HttpResponseMessage response = await _httpClient.PostAsJsonAsync<LoginModel>("api/admin/login", loginModel);
-            ResponseModel result = await response.Content.ReadFromJsonAsync<ResponseModel>();
+    //    //LOGIN REGISTER SERVICE
+    //    public async Task<ResponseModel> Login(LoginModel loginModel)
+    //    {
+    //        HttpResponseMessage response = await _httpClient.PostAsJsonAsync<LoginModel>("api/admin/login", loginModel);
+    //        ResponseModel result = await response.Content.ReadFromJsonAsync<ResponseModel>();
 
-            return result;
-        }
+    //        return result;
+    //    }
 
-        public async Task<ResponseModel> Register(AdminInfo info)
-        {
-            HttpResponseMessage response = await _httpClient.PostAsJsonAsync<AdminInfo>("api/admin/Create", info);
-            if (response.IsSuccessStatusCode)
-            {
-                string content = await response.Content.ReadAsStringAsync();
-                ResponseModel result = JsonConvert.DeserializeObject<ResponseModel>(content);
-                return result;
-            }
-            return null;
-        }
-        //////////////////////////////////////////////////////////////////////////////////////////////////////
-        // DEPARTMENT SERVICE
-        public async Task<Department> CreateDepartment(Department dep)
-        {
-            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("api/department", dep);
-            if (response.IsSuccessStatusCode)
-            {
-                string content = await response.Content.ReadAsStringAsync();
-                Department createdEmp = JsonConvert.DeserializeObject<Department>(content);
-                return createdEmp;
-            }
-            return null;
-        }
+    //    public async Task<ResponseModel> Register(AdminInfo info)
+    //    {
+    //        HttpResponseMessage response = await _httpClient.PostAsJsonAsync<AdminInfo>("api/admin/Create", info);
+    //        if (response.IsSuccessStatusCode)
+    //        {
+    //            string content = await response.Content.ReadAsStringAsync();
+    //            ResponseModel result = JsonConvert.DeserializeObject<ResponseModel>(content);
+    //            return result;
+    //        }
+    //        return null;
+    //    }
+    //    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    //    // DEPARTMENT SERVICE
+    //    public async Task<Department> CreateDepartment(Department dep)
+    //    {
+    //        HttpResponseMessage response = await _httpClient.PostAsJsonAsync("api/department", dep);
+    //        if (response.IsSuccessStatusCode)
+    //        {
+    //            string content = await response.Content.ReadAsStringAsync();
+    //            Department createdEmp = JsonConvert.DeserializeObject<Department>(content);
+    //            return createdEmp;
+    //        }
+    //        return null;
+    //    }
 
-        public async Task<List<Department>> GetAllDepartments()
-        {
-            return await _httpClient.GetFromJsonAsync<List<Department>>("api/department/GetAllDepartments");
-        }
+    //    public async Task<List<Department>> GetAllDepartments()
+    //    {
+    //        return await _httpClient.GetFromJsonAsync<List<Department>>("api/department/GetAllDepartments");
+    //    }
 
-        public async Task<Department> GetDepartment(int id)
-        {
-            return await _httpClient.GetFromJsonAsync<Department>($"api/department/{id}");
-        }
+    //    public async Task<Department> GetDepartment(int id)
+    //    {
+    //        return await _httpClient.GetFromJsonAsync<Department>($"api/department/{id}");
+    //    }
 
-        //////////////////////////////////////////////////////////////////////////////////////////////////////
-        // EMPLOYEE SERVICE
-        public async Task<List<Employee>> GetEmployees()
-        {
-            var result = await _httpClient.GetFromJsonAsync<List<Employee>>("api/employee/GetAllEmployees");
-            return result;
-        }
+    //    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    //    // EMPLOYEE SERVICE
+    //    public async Task<List<Employee>> GetEmployees()
+    //    {
+    //        var result = await _httpClient.GetFromJsonAsync<List<Employee>>("api/employee/GetAllEmployees");
+    //        return result;
+    //    }
 
-        public async Task<Employee> GetEmployeeById(int empID)
-        {
-            return await _httpClient.GetFromJsonAsync<Employee>($"api/employee/{empID}");
-        }
+    //    public async Task<Employee> GetEmployeeById(int empID)
+    //    {
+    //        return await _httpClient.GetFromJsonAsync<Employee>($"api/employee/{empID}");
+    //    }
 
-        public async Task<ResponseModel> CreateEmp(Employee emp)
-        {
-            try
-            {
-                Department department = await GetDepartment(emp.DepartmentId);
+    //    public async Task<ResponseModel> CreateEmp(Employee emp)
+    //    {
+    //        try
+    //        {
+    //            Department department = await GetDepartment(emp.DepartmentId);
 
-                if (department != null)
-                {
-                    emp.Department = department;
+    //            if (department != null)
+    //            {
+    //                emp.Department = department;
 
-                    HttpResponseMessage response = await _httpClient.PostAsJsonAsync("api/employee", emp);
+    //                HttpResponseMessage response = await _httpClient.PostAsJsonAsync("api/employee", emp);
 
-                    if (response.StatusCode == HttpStatusCode.NoContent)
-                    {
-                        return new ResponseModel { Status = true, Message = "Employee created successfully" };
-                    }
-                    else
-                    {
-                        // Handle other status codes
-                        return new ResponseModel { Status = false, Message = $"Failed to create employee. Status code: {response.StatusCode}" };
-                    }
-                }
-                else
-                {
-                    return new ResponseModel { Status = false, Message = "Department not found" };
-                }
-            }
-            catch (Exception ex)
-            {
-                // Log the exception
-                Console.WriteLine($"An error occurred: {ex.Message}");
-                return new ResponseModel { Status = false, Message = "An error occurred while processing your request" };
-            }
-        }
+    //                if (response.StatusCode == HttpStatusCode.NoContent)
+    //                {
+    //                    return new ResponseModel { Status = true, Message = "Employee created successfully" };
+    //                }
+    //                else
+    //                {
+    //                    // Handle other status codes
+    //                    return new ResponseModel { Status = false, Message = $"Failed to create employee. Status code: {response.StatusCode}" };
+    //                }
+    //            }
+    //            else
+    //            {
+    //                return new ResponseModel { Status = false, Message = "Department not found" };
+    //            }
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            // Log the exception
+    //            Console.WriteLine($"An error occurred: {ex.Message}");
+    //            return new ResponseModel { Status = false, Message = "An error occurred while processing your request" };
+    //        }
+    //    }
 
-        public async Task<ResponseModel> UpdateEmp(Employee emp) 
-        {
-            try 
-            {
-                if (emp != null)
-                {
-                    HttpResponseMessage res = await _httpClient.PutAsJsonAsync($"api/employee/{emp.EmpId}", emp);
-                    if (res.StatusCode == HttpStatusCode.NoContent)
-                    {
-                        return new ResponseModel { Status = true, Message = "Employee Updated Successfully" };
-                    }
-                    else
-                    {
-                        // Handle other status codes
-                        return new ResponseModel { Status = false, Message = $"Failed to create employee. Status code: {res.StatusCode}" };
-                    }
-                }
-                else
-                {
-                    return new ResponseModel { Status = false, Message = "Employee not found" };
-                }
-            }
-            catch(Exception ex) 
-            {
-                return null;
-            }
-        }
+    //    public async Task<ResponseModel> UpdateEmp(Employee emp) 
+    //    {
+    //        try 
+    //        {
+    //            if (emp != null)
+    //            {
+    //                HttpResponseMessage res = await _httpClient.PutAsJsonAsync($"api/employee/{emp.EmpId}", emp);
+    //                if (res.StatusCode == HttpStatusCode.NoContent)
+    //                {
+    //                    return new ResponseModel { Status = true, Message = "Employee Updated Successfully" };
+    //                }
+    //                else
+    //                {
+    //                    // Handle other status codes
+    //                    return new ResponseModel { Status = false, Message = $"Failed to create employee. Status code: {res.StatusCode}" };
+    //                }
+    //            }
+    //            else
+    //            {
+    //                return new ResponseModel { Status = false, Message = "Employee not found" };
+    //            }
+    //        }
+    //        catch(Exception ex) 
+    //        {
+    //            return null;
+    //        }
+    //    }
 
-        public async Task<ResponseModel> DeleteEmp(int id)
-        {
-            try 
-            {
-                if(id!=null) 
-                {
-                    HttpResponseMessage res = await _httpClient.DeleteAsync($"api/employee/{id}");
+    //    public async Task<ResponseModel> DeleteEmp(int id)
+    //    {
+    //        try 
+    //        {
+    //            if(id!=null) 
+    //            {
+    //                HttpResponseMessage res = await _httpClient.DeleteAsync($"api/employee/{id}");
 
-                    if(res.StatusCode == HttpStatusCode.NoContent) 
-                    {
-                        return new ResponseModel { Status = true, Message = "Employee Updated Successfully" };
-                    }  
-                    else
-                    {
-                        return new ResponseModel { Status = false, Message = $"Failed to create employee. Status code: {res.StatusCode}" };
-                    }
-                }
-                else 
-                {
-                    return new ResponseModel { Status = false, Message = "Employee not found" };
-                }
-            } 
-            catch(Exception ex) 
-            {
-                return null;
-            }
+    //                if(res.StatusCode == HttpStatusCode.NoContent) 
+    //                {
+    //                    return new ResponseModel { Status = true, Message = "Employee Updated Successfully" };
+    //                }  
+    //                else
+    //                {
+    //                    return new ResponseModel { Status = false, Message = $"Failed to create employee. Status code: {res.StatusCode}" };
+    //                }
+    //            }
+    //            else 
+    //            {
+    //                return new ResponseModel { Status = false, Message = "Employee not found" };
+    //            }
+    //        } 
+    //        catch(Exception ex) 
+    //        {
+    //            return null;
+    //        }
 
-        }
-        //////////////////////////////////////////////////////////////////////////////////////////////////////
-        // ADDRESS SERVICE
-        public async Task<List<Address>> GetAddresses()
-        {
-            var addresses = await _httpClient.GetFromJsonAsync<List<Address>>("api/address/GetAllAddress");
-            return addresses;
-        }
+    //    }
+    //    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    //    // ADDRESS SERVICE
+    //    public async Task<List<Address>> GetAddresses()
+    //    {
+    //        var addresses = await _httpClient.GetFromJsonAsync<List<Address>>("api/address/GetAllAddress");
+    //        return addresses;
+    //    }
 
-        public async Task<Address> GetAddressById(int addressId) 
-        {
-            var addId =  await _httpClient.GetFromJsonAsync<Address>($"api/address/{addressId}");
-            if(addId!=null)
-            {
-                return addId;
-            }
-            return null;
-        }
-    }
+    //    public async Task<Address> GetAddressById(int addressId) 
+    //    {
+    //        var addId =  await _httpClient.GetFromJsonAsync<Address>($"api/address/{addressId}");
+    //        if(addId!=null)
+    //        {
+    //            return addId;
+    //        }
+    //        return null;
+    //    }
+    //}
 }
--- Employee_Manager_Client/Services/IFrontendUserPanelService.cs
+++ Employee_Manager_Client/Services/IFrontendUserPanelService.cs
@@ -1,7 +1,33 @@
-﻿namespace Employee_Manager_Client.Services
+﻿using Employee_Manager_Models;
+using Employee_Manager_Models.CustomModels;
+using Employee_Manager_Models.Models;
+
+namespace Employee_Manager_Client.Services
 {
     public interface IFrontendUserPanelService
     {
+        //user status
         Task<bool> IsUserLoggedIn();
+
+        //access service
+        Task<ResponseModel> Login(LoginModel loginModel);
+        Task<ResponseModel> Register(AdminInfo info);
+
+        //department service
+        Task<Department> CreateDepartment(Department dep);
+        Task<List<Department>> GetAllDepartments();
+        Task<Department> GetDepartment(int id);
+
+        //employee service
+        Task<List<Employee>> GetEmployees();
+        Task<Employee> GetEmployeeById(int empID);
+        Task<ResponseModel> CreateEmp(Employee emp);
+        Task<ResponseModel> UpdateEmp(Employee emp);
+        Task<ResponseModel> DeleteEmp(int id);
+
+        //address service
+        Task<List<Address>> GetAddresses();
+        Task<Address> GetAddressById(int addressId);
+
     }
 }
