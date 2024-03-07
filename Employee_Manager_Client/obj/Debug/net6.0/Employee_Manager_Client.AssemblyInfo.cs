c7b3005383d7333e832940f78f310929d9ea41cc
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-03-07 21:32:03 +0530

    edit file restored to original due to issues

--- Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
+++ Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8569468a0be3e8ae6b05f68e297f3a91edca143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a95f13b7b2f06deb6faafd9c10f9a217dec4252e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
