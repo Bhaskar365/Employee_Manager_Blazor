9ed178e2569b2e5f939eeb25204552a2edead7b3
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-03-12 22:01:42 +0530

    export to excel code test

--- Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
+++ Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48ca1d70b92ef21cca36994b6d05477732975fd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
