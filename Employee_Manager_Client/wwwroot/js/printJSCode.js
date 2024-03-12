539af884575a9138eccadcd48da5dccc74e893a3
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-03-12 22:28:55 +0530

    export code added but not working

--- Employee_Manager_Client/wwwroot/js/printJSCode.js
+++ Employee_Manager_Client/wwwroot/js/printJSCode.js
@@ -1,3 +1,14 @@
 ﻿function PrintBtn() {
     window.print();
 }
+
+window.BlazorDownloadFile = (fileName, data) => {
+    const blob = base64toBlob(data, 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    document.body.appendChild(a);
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(url);
+};
