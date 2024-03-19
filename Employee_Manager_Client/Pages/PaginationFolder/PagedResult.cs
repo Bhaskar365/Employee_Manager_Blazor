87f8850cf7b1997abee16e2191a954162a7ed89c
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-03-19 19:52:58 +0530

    changed pagination type in homepage

--- Employee_Manager_Client/Pages/PaginationFolder/PagedResult.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Employee_Manager_Client.Pages.PaginationFolder
-{
-    public class PagedResult<T> : PagedResultBase where T : class
-    {
-        public T[]? Results { get; set; }
-    }
-}
