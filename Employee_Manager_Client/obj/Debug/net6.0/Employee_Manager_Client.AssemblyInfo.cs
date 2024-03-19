3caf63ae91e5b0d7755ce6daa9609b06c67e8dc2
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-03-19 21:50:20 +0530

    pagination css for small screen changes

--- Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
+++ Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f8850cf7b1997abee16e2191a954162a7ed89c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e08a4e444a4e10e4d722c708845d21b6e68037ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
