f829f5a9d4591edd36aa0574882237281d4c8c5e
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-04-04 22:25:17 +0530

    razor component file cleanup

--- Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
+++ Employee_Manager_Client/obj/Debug/net6.0/Employee_Manager_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f4a551666616ca4e9d822c4fd0850e15ee0ce6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Employee_Manager_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
